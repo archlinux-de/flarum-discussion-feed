7bcfbc88cf66572500553e382873ea3e126cdba0
Pierre Schmitz <pierre@archlinux.de>
2021-08-21 18:13:24 +0200

    Add feed link to document head

--- extend.php
+++ extend.php
@@ -5,6 +5,7 @@ namespace ArchLinux\DiscussionFeed;
 use ArchLinux\DiscussionFeed\Console\CreateDiscussionFeed;
 use ArchLinux\DiscussionFeed\Controller\FeedController;
 use Flarum\Extend;
+use Flarum\Frontend\Document;
 use Illuminate\Console\Scheduling\Event;
 
 return [
@@ -16,4 +17,8 @@ return [
         }),
     (new Extend\View())->namespace('discussion-feed', __DIR__ . '/views'),
     (new Extend\Routes('forum'))->get('/feed.xml', 'discussion-feed.feed', FeedController::class),
+    (new Extend\Frontend('forum'))
+        ->content(function (Document $document) {
+            $document->head[] = '<link href="/feed.xml" type="application/atom+xml" rel="alternate" title="Discussion Atom feed" />';
+        })
 ];
